073e38d688ea4bec9aa869dbd307a393e05ea4ed
David Heidari <davidheidari2@gmail.com>
2024-05-31 09:22:49 +0200

    added some more file and deleted user-mapp

--- app.mjs
+++ app.mjs
@@ -1,22 +1,16 @@
-/* Du ska utveckla ett RESTful WEB API för en klädesbutik.
-Butiken ska innehålla fem olika typer av varor: T - shirts,
- Tröjor, Strumpor, Jackor och Byxor.För varje varutyp ska det finnas 
- minst fem olika varor.API:et ska kunna
- hantera följande funktioner */
 // 1. Varor:
 // · Lägg till nya varor.
 // · Uppdatera hela eller delar av en specifik vara.
 // · Ta bort en specifik vara.
 
-
-import oExpress from "express";
+import express from "express";
 import fs from "fs";
 const PORT = 3000;
-const app = oExpress();
+const app = express();
 
-const dataJson = JSON.parse(fs.readFileSync("./clothes-store/inventory.json"));
+const dataJson = JSON.parse(fs.readFileSync("inventory.json"));
 
-app.use(oExpress.json());
+app.use(express.json());
 
 //** GET
 app.get("/", (_req, _res) => {
@@ -24,21 +18,16 @@ app.get("/", (_req, _res) => {
 })
 
 //** GET
+//* http://localhost:3000/api/products
 app.get("/api/products", (_req, _res) => {
-    return _res.status(200).json({
-        status: "sucess",
-        // count: dataJson.length,
-        data: {
-            dataJson: dataJson
-        }
-    });
+    return _res.status(200).json({dataJson});
 })
 
 //** POST
 app.post("/api/products", (_req, _res) => {
-    let { body } = _req
-    let addNewProduct = { id: dataJson[dataJson.length - 1].id + 1, ...body }
-    
+    let { body } = _req;
+    let addNewProduct = { id: dataJson[dataJson.length - 1].id + 1, ...body };
+
     dataJson.push(addNewProduct);
     _res.status(201).send(addNewProduct);
 })
@@ -46,25 +35,22 @@ app.post("/api/products", (_req, _res) => {
 //** PUT
 app.put("/api/products/:id", (_req, _res) => {
     // Vi uppdaterar body och använder params för att hitta det vi ska uppdatera :) 
-    let { body, params: { id } } = _req
+    let { body, params: { id } } = _req;
     
-    // ser till så att id är endast ett nummer
-    let productId = parseInt(id)
+    let productId = parseInt(id);
     if (isNaN(id)) {
         return _response.status(400).send('400: Invalid. Bad request');
     }
 
-    // hittar rätt index i Json filen
     let productIndex = dataJson.findIndex((_product) => {
-        return _product.id === productId
+        return _product.id === productId;
     })
 
-    // felmeddelande om index inte finns
     if (productIndex == -1) {
         return _res.status(404).send("404: Page not found");
     }
 
-    dataJson[productIndex] = { id: productId, ...body }
+    dataJson[productIndex] = { id: productId, ...body };
     
     _res.status(200).send(dataJson[productIndex]);
 
@@ -73,17 +59,17 @@ app.put("/api/products/:id", (_req, _res) => {
 //** PATCH
 app.patch("/api/products/:id", (_req, _res) => {
     // Vi uppdaterar body och använder params för att hitta det vi ska uppdatera :) 
-    let { body, params: { id } } = _req
+    let { body, params: { id } } = _req;
 
     // ser till så att id är endast ett nummer
-    let productId = parseInt(id)
+    let productId = parseInt(id);
     if (isNaN(id)) {
         return _response.status(400).send('400: Invalid. Bad request');
     }
 
     // hittar rätt index i Json filen
     let productIndex = dataJson.findIndex((_product) => {
-        return _product.id === productId
+        return _product.id === productId;
     })
 
     // felmeddelande om index inte finns
@@ -91,7 +77,7 @@ app.patch("/api/products/:id", (_req, _res) => {
         return _res.status(404).send("404: Page not found");
     }
 
-    dataJson[productIndex] = { ...dataJson[productIndex], ...body }
+    dataJson[productIndex] = { ...dataJson[productIndex], ...body };
 
     _res.status(200).send(dataJson[productIndex]);
 
@@ -99,16 +85,16 @@ app.patch("/api/products/:id", (_req, _res) => {
 
 //** DELETE
 app.delete("/api/products/:id", (_req, _res) => {
-    let { params: { id } } = _req
+    let { params: { id } } = _req;
     
-    let productId = parseInt(id)
+    let productId = parseInt(id);
     if (isNaN(id)) {
-        return _res.status(400).send("400: invalid. Bad request")
+        return _res.status(400).send("400: invalid. Bad request");
     }
 
     // hittar rätt index i Json filen
     let productIndex = dataJson.findIndex((_product) => {
-        return _product.id === productId
+        return _product.id === productId;
     })
 
     // felmeddelande om index inte finns
@@ -117,12 +103,12 @@ app.delete("/api/products/:id", (_req, _res) => {
     }
 
     // tar bort från jsonobjektet, väljer vilken och hur många
-    dataJson.splice(productIndex, 1)
+    dataJson.splice(productIndex, 1);
 
     _res.sendStatus(204);
 })
 
 
 app.listen(PORT, () => {
-    console.log(`App is running on localhost: ${PORT}`)
+    console.log(`App is running on localhost: ${PORT} http://localhost:3000`);
 })
